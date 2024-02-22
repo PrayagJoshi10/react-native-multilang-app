7ddd091e4d518bbdb2102279aebac0fc3828a979
Prayag Joshi <prayagjoshi263@gmail.com>
2024-02-22 17:07:16 +0530

    Implemented language change logic

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
 import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import i18next from './services/i18next';
+import {useTranslation} from 'react-i18next';
 
 const App = () => {
+  const {t} = useTranslation();
+
+  const changeLng = (lng: string) => {
+    i18next.changeLanguage(lng);
+  };
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
-      <Text style={styles.title}>Welcome</Text>
+      <Text style={styles.title}>{t('welcome')}</Text>
       <View style={styles.buttonContainer}>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button} onPress={() => changeLng('en')}>
           <Text style={styles.buttonLabel}>English</Text>
         </Pressable>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button} onPress={() => changeLng('hi')}>
           <Text style={styles.buttonLabel}>Hindi</Text>
         </Pressable>
-        <Pressable style={styles.button}>
+        <Pressable style={styles.button} onPress={() => changeLng('gu')}>
           <Text style={styles.buttonLabel}>Gujarati</Text>
         </Pressable>
       </View>
